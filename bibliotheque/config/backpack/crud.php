92897f7bea484ab1922ae53ce91773a42d85abba
John FOLLY <follyjohn@outlook.fr>
2020-10-19 13:14:54 +0000

    cgvbn

--- bibliotheque/config/backpack/crud.php
+++ bibliotheque/config/backpack/crud.php
@@ -282,7 +282,7 @@ return [
         // "en_GB" => "English (United Kingdom)",
         // "en_US" => "English (United States)",
         // "en_ZW" => "English (Zimbabwe)",
-        // 'en' => 'English',
+        'en' => 'English',
         // "eo" => "Esperanto",
         // "et_EE" => "Estonian (Estonia)",
         // "et" => "Estonian",
